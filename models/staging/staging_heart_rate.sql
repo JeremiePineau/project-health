46f46a8fe0cf712ebfe1b46aa5db4dce97594f71
jeremie.pineau93@gmail.com <jeremie.pineau93@gmail.com>
2021-07-14 15:44:15 +0000

    staging_hr_first_vers

--- models/staging/staging_heart_rate.sql
+++ models/staging/staging_heart_rate.sql
@@ -1,3 +1,7 @@
+{{ config (
+    materialized="view"
+)}}
+
 with cte_heart_rate as (
     select 
         date_ts
@@ -6,5 +10,21 @@ with cte_heart_rate as (
     from dbt_project_health.raw_heart_rate
 )
 
+, date_formatted as (
+    select 
+        format_timestamp('%Y-%m-%d %H:%M:%S', cast(concat(cast(date_ts as string), " ", cast(hours as string)) as timestamp)) as date_ts
+        , regexp_replace(cast(date_ts as string), "-","") as dateid
+        , heart_rate
+    from cte_heart_rate
+)
+
+, last_ts as (
+    select *
+        , coalesce(lag(date_ts) over(order by date_ts), date_ts) as last_date_ts 
+    from date_formatted
+)
+
 select *
-from cte_heart_rate
+, unix_seconds(cast(date_ts as timestamp)) - unix_seconds(cast(last_date_ts as timestamp)) as seconds_diff 
+from last_ts
+order by date_ts
