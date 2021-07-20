0fdf209b42e24211bcc93d6178d3f227176f4ce3
jeremie.pineau93@gmail.com <jeremie.pineau93@gmail.com>
2021-07-20 00:51:46 +0000

    added fix for N/A in weights workout logs

--- models/staging/staging_workout_logs.sql
+++ models/staging/staging_workout_logs.sql
@@ -4,7 +4,11 @@
 
 with cte_workout_logs as (
     select 
-        *
+        timestamp
+        , exercice
+        , cast(if(weight = "N/A","0",weight) as integer) as weight
+        , reps
+        , sets
     from {{source('dbt_project_health', 'workout_logs')}}
 )
 
